cd73ab996c947da8a2a8308e00fbfbe853c6d48a
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-21 17:52:51 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,10 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |name, data|
+    data.each do |attribute, value|
+      if attribute == :favorite_ice_cream_flavors
+        :favorite_ice_cream_flavors.drop(1)
+      else 
+      end
 end
